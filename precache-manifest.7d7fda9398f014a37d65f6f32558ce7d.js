6c8066fc741c13587d0555b1ed0024e3edac67ce
BarryShovlin <bshovlin32@gmail.com>
2021-12-29 16:41:05 -0600

    Updates

---
+++ precache-manifest.7d7fda9398f014a37d65f6f32558ce7d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9ee132a166c18e2fad0188d38dfe94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aecc0e63d8cdce7a672",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "a5a4193232230bec42bc",
+    "url": "/static/js/2.e8a5c3ff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.e8a5c3ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aecc0e63d8cdce7a672",
+    "url": "/static/js/main.2c98c910.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "98f806aec4bdb33217de54a53a003093",
+    "url": "/static/media/headshot.98f806ae.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
